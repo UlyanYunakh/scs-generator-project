381f10155728b09a417689a280a5e4e32e93f46a
UlyanYunakh <ulyanyunakh@gmail.com>
2022-02-06 13:27:07 +0300

    query constructor
    -  createQuery method

--- src/osm-module/query-constructor.js
+++ src/osm-module/query-constructor.js
@@ -1,6 +1,22 @@
+import { OSMQuery } from "@toriyama/osmql";
+
 export function QueryConstructor() {
+    /**
+     * 
+     * @param {string} areaId 
+     * @param {Array<any>} tags 
+     * @returns {OSMQuery}
+     */
     this.CreateQuery = (areaId, tags) => {
-        
-        return '';
+        const tagsString = tags.map(tag => {
+            if (!tag.name) {
+                throw "Tag must have name";
+            }
+            return tag.value ? `[${tag.name}=${tag.value}]` : `[${tag.name}]`;
+        }).join('');
+
+        let query = `area(id:${areaId})->.searchArea;(node${tagsString}(area.searchArea););out;`;
+
+        return new OSMQuery().fromQLString(query);
     };
 }
