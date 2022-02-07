7a76e64066e383c842cdb8b8f8815baa8f01ee25
YanaGv <gv.yanina@gmail.com>
2022-02-07 21:11:47 +0300

    add museum property template

--- src/script-config.js
+++ src/script-config.js
@@ -1,35 +1,60 @@
 export const config = {
     mainTemplate: (...args) => {
-        let result = "";
-        result += args[0] + " <- concept_museum;\n";
-        
+        let result =
+            `${args[0]} <- concept_museum;\n` +
+            `=> nrel_main_idtf:\n` +
+            `   [${args[1]}]\n` +
+            `   (* <- lang_en;; <- name_en;;*);\n` +
+            `   [${args[2]}]\n` +
+            `   (* <- lang_ru;; <- name_ru;; <- name;;*);`;
         return result;
     },
     propertyTemplates: [
         {
             osmPropertyName: 'phone',
             scsPropertyTemplate: (...args) => {
-                return `=>nrel_phone_number:
-                [${args[0]}]
-                (*<-concept_phone_number;;*);`;
+                let result = `\n=> nrel_phone_number:`;
+                args.forEach(phone => {
+                    result += `\n` +
+                    `   [${phone}]\n` +
+                    `   (* <- concept_phone_number;;*);`
+                });
+                return result;
             },
         },
         {
             osmPropertyName: 'site',
             scsPropertyTemplate: (...args) => {
-                return ``;
+                let result = `\n=> nrel_site:`;
+                args.forEach(site => {
+                    result += `\n` +
+                        `   [${site}]\n` +
+                        `   (*<-concept_site;;*);`
+                });
+                return result;
             },
         },
         {
             osmPropertyName: 'email',
             scsPropertyTemplate: (...args) => {
-                return ``;
+                let result = `\n=> nrel_email:`;
+                args.forEach(email => {
+                    result += `\n` +
+                        `   [${email}]\n` +
+                        `   (*<-concept_email;;*);`
+                });
+                return result;
             },
         },
         {
             osmPropertyName: 'region',
             scsPropertyTemplate: (...args) => {
-                return ``;
+                let result = `\n=> nrel_region:`;
+                args.forEach(region => {
+                    result += `\n` +
+                        `   [${region}];`
+                });
+                return result;
             },
         }
     ],
